e998685a9e28d8a4290fcdd19bbaf7ada94b3ff0
MartinNava <martin.nava1712@gmail.com>
2022-09-06 14:15:50 -0500

    first front

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
 app.get('/api', (request, response) => {
     response.send('API Correcta');
 });
